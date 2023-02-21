0eb5382fdce52a9300d88e8447a5b59d99c68b6b
Steven Gangl <steven.h.gangl@gmail.com>
2023-02-21 13:42:52 -0600

    our first attempt at cracking this

--- client.js
+++ client.js
@@ -43,15 +43,33 @@ console.log('array of employee data: ',  employees );
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-
-
-
 // This function will calculate 1 employee's bonus!
 //
 function calculateIndividualEmployeeBonus( employee ) {  
-  // your logic here
-  
-  
-  // return new object with bonus results
-
+  let money = employees.reviewRating;
+  let eNumber = employees.employeeNumber
+//   Those who have a rating of a 2 or below should not receive a bonus.
+for()
+if(money <= 2){
+  console.log("Do not give bonus");
 }
+// Those who have a rating of a 3 should receive a base bonus of 4% of their base annual income.
+else if(money === 3){
+  console.log()// 4% of annualSalary
+}
+// Those who have a rating of a 4 should receive a base bonus of 6% of their base annual income.
+else if (money === 4){
+  console.log() // 6%
+}
+// Those who have a rating of a 5 should receive a base bonus of 10% of their base annual income.
+else if (money === 5){
+  console.log() //10%
+}
+// If their employee number is 4 digits long, this means they have been with the company for longer than 15 years, and should receive an additional 5%.
+if(eNumber ===)
+// However, if their annual income is greater than $65,000, they should have their bonus adjusted down 1%.
+
+// No bonus can be above 13% or below 0% total.
+}
+employees.bonusPrecentage = 0
+calculateIndividualEmployeeBonus('atticus')
